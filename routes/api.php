ccb24bda3f6eb36c35ed62b9d21b60e93154758f
Nur Rachmat <rachmat.nur91@gmail.com>
2021-05-22 18:49:00 +0700

    Belajar membuat API dan Controller (Resource Controller)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/", function(){
+    return array("status" => true, "pesan"=> "Hallo API");
+});
+
+Route::get("/hallo/{nama?}/{npm?}", function($nama = "John Doe", $npm = NULL){
+    $mhs = array(
+        "nama"  => $nama,
+        "npm"   => $npm
+    );
+    return array(
+        "status" => true, 
+        "pesan" => "Hallo, Nama Saya $nama",
+        "mhs"   => $mhs
+    );
+});
+
+Route::get("/home", [HomeController::class, "index"]);
+Route::get("/home/kenalan/{nama?}", [HomeController::class, "kenalan"]);
+
+//Buat sebuah API Controller/Resource Controller untuk mengolah data Mahasiswa
+//Nama Controller nya adalah MahasiswaController
+//Daftarkan Resource Controller :MahasiswaController ke path mahasiswa
+Route::apiResource("mahasiswa", MahasiswaController::class);
